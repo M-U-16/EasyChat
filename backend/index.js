de7b5fd7fac4435da3407f5bc6cc648ab80470cf
M-U-16 <132483300+M-U-16@users.noreply.github.com>
2024-11-07 22:17:48 +0100

    cleanup .sock file on SIGINT

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 import fs from "fs"
 import "dotenv/config"
+
 if (process.env.SOCKET_PATH) {
-    const path = "/var/tmp/"+process.env.SOCKET_PATH
+    const path = process.env.SOCKET_PATH
     fs.existsSync(path) && fs.unlinkSync(path)
 }
+
 import cors from "cors"
 import http from "http"
 import express from "express"
@@ -24,6 +26,16 @@ const app = express()
 const server = http.createServer(app)
 //creating websocket server on top of http server
 export const io = new SocketIoServer(server, socketConf)
+
+process.on("SIGINT", () => {
+    server.close(()=>{
+        console.log("Server Closed")
+        if (process.env.SOCKET_PATH) {
+            fs.unlinkSync(process.env.SOCKET_PATH)
+            process.exit(0)
+        }
+    })
+})
 
 //middleware and configuration
 app.use(express.json())
