0aa03493bde1642e51e619ba69d50e8b53467356
M-U-16 <132483300+M-U-16@users.noreply.github.com>
2024-12-06 19:08:43 +0100

    removing canvas

--- backend/src/helpers/user.create-picture.js
+++ backend/src/helpers/user.create-picture.js
@@ -1,29 +1,35 @@
 import fs from "fs"
 import "dotenv/config"
-import { createCanvas } from "canvas";
 import path from "path";
+import * as PImage from "pureimage"
+
+const font = PImage.registerFont(
+    "../../assets/Roboto/Roboto-Medium.ttf",
+    "Roboto"
+)
+font.loadSync()
 
 const getRandom255 = () => Math.floor(Math.random() * 255)
 const getRandomColor = () => `rgb(${getRandom255()},${getRandom255()},${getRandom255()})`
 
 function create64x64Profile(firstLetter, path) {
-    const canvas = createCanvas(64, 64)
+    const canvas = PImage.make(64, 64)
     const context = canvas.getContext("2d")
-    context.font = "bold 20pt 'Verdana'"
+    context.font = "20pt Roboto"
     context.textAlign = "center"
     context.fillStyle = getRandomColor()
     context.fillRect(0, 0, 64, 64)
     context.fillStyle = "#fff"
     context.fillText(firstLetter, 32, 43)
-    const buffer = canvas.toBuffer("image/png")
-    fs.writeFileSync(path, buffer)
+    return PImage.encodePNGToStream(canvas, fs.createWriteStream(path))
 }
 
-process.on("message", message => {
+process.on("message", async(message) => {
     create64x64Profile(
         message.username[0],
         path.join(message.userDir, "profile.png")
-    )
-    process.send("done")
-    process.exit(0)
+    ).then(()=>{
+        process.send("done")
+        process.exit(0)
+    })
 })
